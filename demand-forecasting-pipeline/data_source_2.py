72ffb2c42539edfacb9e079dafedf0cf32f61a79
AyoDennis <ayodejidennis33@gmail.com>
2025-05-23 13:12:46 +0100

    data source 2 logging

--- demand-forecasting-pipeline/data_source_2.py
+++ demand-forecasting-pipeline/data_source_2.py
@@ -1,7 +1,10 @@
 import json
 import random
+import logging
 from data_source_1 import fake
 
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(20)
 
 def simulate_api_data(n=1000):
     return [
