c1a061de5f606524653445d77ca4237d1de50868
AyoDennis <ayodejidennis33@gmail.com>
2025-05-23 13:26:49 +0100

    data source 3

---
+++ demand-forecasting-etl/data_source_3.py
@@ -0,0 +1,9 @@
+import sqlite3
+import json
+import random
+import logging
+from data_source_1 import fake
+
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(20)
+
