c79d50701325074ca9e180089ec9f9603dd00c67
AyoDennis <ayodejidennis33@gmail.com>
2025-05-23 15:25:08 +0100

    etl script

--- demand-forecasting-etl/airflow/modules/etl.py
+++ demand-forecasting-etl/airflow/modules/etl.py
@@ -14,7 +14,7 @@ def extract_csv():
     """
     df = pd.read_csv("../mock_supplier_data.csv")
     logging.info("csv read")
-    df.to_pickle("/tmp/df_csv.pkl")
+    df.to_pickle("./tmp/df_csv.pkl")
 
 logging.info("csv data source pickled")
 
@@ -27,7 +27,7 @@ def extract_api():
     logging.info("api data read")
     df = pd.json_normalize(data)
     logging.info("api json data normalized")
-    df.to_pickle("/tmp/df_api.pkl")
+    df.to_pickle("./tmp/df_api.pkl")
 
 logging.info("api data source pickled")
 
@@ -41,7 +41,7 @@ def extract_sql():
     logging.info("queried db source")
     conn.close()
     logging.info("closed db connection")
-    df.to_pickle("/tmp/df_sql.pkl")
+    df.to_pickle("./tmp/df_sql.pkl")
 
 logging.info("db source pickled")
 
@@ -49,22 +49,26 @@ def transform_and_merge():
     """
     Converts all pickled data sources into daaframes
     """
-    df_csv = pd.read_pickle("/tmp/df_csv.pkl")
+    df_csv = pd.read_pickle("./tmp/df_csv.pkl")
     logging.info("pickled csv converted into dataframe")
-    df_api = pd.read_pickle("/tmp/df_api.pkl")
+    df_api = pd.read_pickle("./tmp/df_api.pkl")
     logging.info("pickled api data converted into dataframe")
-    df_sql = pd.read_pickle("/tmp/df_sql.pkl")
+    df_sql = pd.read_pickle("./tmp/df_sql.pkl")
     logging.info("pickled db data converted into dataframe")
 
     df_csv.rename(columns={"supplier_id": "supplier_uuid"}, inplace=True)
+    logging.info("csv df columns names transformed for consistency")
     df_sql.rename(columns={"supplier_id": "supplier_uuid"}, inplace=True)
+    logging.info("sql df columns names transformed for consistency")
 
     df_merged = df_csv.merge(df_api, on="supplier_uuid", how="outer") \
                       .merge(df_sql, on="supplier_uuid", how="outer")
     df_merged.fillna("N/A", inplace=True)
+    logging.info("csv, db and api columns names merged for consistency")
 
-    output_path = f"/tmp/merged_supplier_data.csv"
+    output_path = f"./merged_supplier_data.csv"
     df_merged.to_csv(output_path, index=False)
+    logging.info("all data sources successfully merged")
 
 
 # def upload_to_s3():
