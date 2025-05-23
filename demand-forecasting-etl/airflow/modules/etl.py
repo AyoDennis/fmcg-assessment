fb88212d13f84de4e13356ea79f4445c46e1d59e
AyoDennis <ayodejidennis33@gmail.com>
2025-05-23 14:27:38 +0100

    adding modules

--- demand-forecasting-etl/airflow/modules/etl.py
+++ demand-forecasting-etl/airflow/modules/etl.py
@@ -23,3 +23,19 @@ def extract_sql():
     df.to_pickle("/tmp/df_sql.pkl")
 
 
+def transform_and_merge():
+    df_csv = pd.read_pickle("/tmp/df_csv.pkl")
+    df_api = pd.read_pickle("/tmp/df_api.pkl")
+    df_sql = pd.read_pickle("/tmp/df_sql.pkl")
+
+    df_csv.rename(columns={"supplier_id": "supplier_uuid"}, inplace=True)
+    df_sql.rename(columns={"supplier_id": "supplier_uuid"}, inplace=True)
+
+    df_merged = df_csv.merge(df_api, on="supplier_uuid", how="outer") \
+                      .merge(df_sql, on="supplier_uuid", how="outer")
+    df_merged.fillna("N/A", inplace=True)
+
+    output_path = f"/tmp/merged_supplier_data.csv"
+    df_merged.to_csv(output_path, index=False)
+
+
