41a9b73ce4dc4a41acabc7f31327cceb0ca1d32f
AyoDennis <ayodejidennis33@gmail.com>
2025-05-23 13:27:07 +0100

    data source 3

--- demand-forecasting-etl/data_source_3.py
+++ demand-forecasting-etl/data_source_3.py
@@ -7,3 +7,15 @@ from data_source_1 import fake
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
 logging.getLogger().setLevel(20)
 
+conn = sqlite3.connect('suppliers.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE supplier_records (
+    supplier_id TEXT PRIMARY KEY,
+    category TEXT,
+    rating REAL,
+    last_order_date TEXT
+)
+''')
+
