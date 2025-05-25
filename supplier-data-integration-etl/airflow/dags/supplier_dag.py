e16f89e5f29c2f896d797f5e7fb44c6f35b06e6f
AyoDennis <ayodejidennis33@gmail.com>
2025-05-25 18:20:40 +0100

    etl

---
+++ supplier-data-integration-etl/airflow/dags/supplier_dag.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from etl import (extract_api, extract_csv, extract_sql, transform_and_merge,
+                 upload_to_s3)
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 5
+}
+
+with DAG(
+    dag_id='supplier_data_etl_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+
+    task_extract_csv = PythonOperator(
+        task_id='extract_csv_data',
+        python_callable=extract_csv
+    )
+
+    task_extract_api = PythonOperator(
+        task_id='extract_api_data',
+        python_callable=extract_api
+    )
+
+    task_extract_sql = PythonOperator(
+        task_id='extract_sql_data',
+        python_callable=extract_sql
+    )
+
+    task_transform = PythonOperator(
+        task_id='transform_and_merge',
+        python_callable=transform_and_merge
+    )
+
+    task_upload_s3 = PythonOperator(
+        task_id='upload_merged_to_s3',
+        python_callable=upload_to_s3
+    )
+
+    [task_extract_csv, task_extract_api, task_extract_sql] >> task_transform >> task_upload_s3
---
+++ supplier-data-integration-etl/airflow/utils/data_generation/data_source_3.py
@@ -0,0 +1,44 @@
+import json
+import logging
+import random
+import sqlite3
+
+from faker import Faker
+
+fake = Faker()
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(20)
+
+conn = sqlite3.connect('./suppliers.db')
+cursor = conn.cursor()
+
+logging.info("cursor connected")
+
+cursor.execute("DROP TABLE IF EXISTS supplier_records")
+
+cursor.execute('''
+CREATE TABLE supplier_records (
+    supplier_id TEXT PRIMARY KEY,
+    category TEXT,
+    rating REAL,
+    last_order_date TEXT
+)
+''')
+
+logging.info("db schema created")
+
+for _ in range(1000):
+    cursor.execute('''
+    INSERT INTO supplier_records VALUES (?, ?, ?, ?)
+    ''', (
+        fake.uuid4(),
+        random.choice(["Raw Materials", "Logistics", "Manufacturing"]),
+        round(random.uniform(1.0, 5.0), 2),
+        fake.date_between(start_date='-2y', end_date='today').isoformat()
+    ))
+
+
+conn.commit()
+logging.info("connection commmitted")
+conn.close()
+logging.info("connection closed")
